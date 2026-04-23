577289c8872e1570c22ff10f09c5e647895e093c
Harmesh15 <b180050@nitsikkim.ac.in>
2026-04-23 16:43:45 +0530

    personal message setup

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const http = require("http");
 const sequelize = require("./utils/db-connection"); 
-const {Server} = require("socket.io");
+// const {Server} = require("socket.io");
 const path = require("path");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const chatusers = require("./models/chatusers");
+// const chatusers = require("./models/chatusers");
+
+
+const socketIo = require("./socket_io/index")
 
 
 const userRouter = require("./routes/loginSignupRot");
@@ -16,12 +19,14 @@ const messageRoute = require("./routes/messagesRot");
 
 const app = express();
 const server = http.createServer(app);
+socketIo(server);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*"
+//   }
+// });
 
 
 // ================== MIDDLEWARE ==================
@@ -33,49 +38,30 @@ app.use(express.static("public"));
 
 // after socket///////////////////
 
-io.use( async (socket, next) => {
-  console.log("middlw ware backend run")
-  try{
-        const token = socket.handshake.auth.token;
+// io.use( async (socket, next) => {
+//   console.log("middlw ware backend run")
+//   try{
+//         const token = socket.handshake.auth.token;
 
-        if(!token){
-         return next(new Error("Token is missing"));
-      }
-    const decode = jwt.verify(token, "secretkey"); 
-    const user = await chatusers.findByPk(decode.userId);
-    console.log("USER:", user);
-    console.log("this is user for username",user)
+//         if(!token){
+//          return next(new Error("Token is missing"));
+//       }
+//     const decode = jwt.verify(token, "secretkey"); 
+//     const user = await chatusers.findByPk(decode.userId);
+//     console.log("USER:", user);
+//     console.log("this is user for username",user)
 
-      if(!user){
-         return next( new Error("user not found"));
-      }
-      socket.user = user;
-      next();
-   }catch(error){
-    console.log("SOCKET ERROR:", error);
-         return next( new Error("Internal server error"));
-  }
-});
+//       if(!user){
+//          return next( new Error("user not found"));
+//       }
+//       socket.user = user;
+//       next();
+//    }catch(error){
+//     console.log("SOCKET ERROR:", error);
+//          return next( new Error("Internal server error"));
+//   }
+// });
 
-io.on('connection',(socket)=>{
-    console.log("Connected:", socket.user.name);
-
-    socket.on("send_message",(msg)=>{
-        console.log("message", msg);
-
-        const data = {
-            message: msg.message,
-            id: socket.user.id,
-            name: socket.user.name
-        };
-
-        io.emit("receive_message", data);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("disconnected");
-    });
-});
 
 
 
